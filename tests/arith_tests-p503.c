b13dd3cca189593d1fc46cd1bae2e70d5c202eb9
Patrick Longa <plonga@microsoft.com>
2018-07-23 17:20:50 -0700

    Update arith_tests-p503.c

--- tests/arith_tests-p503.c
+++ tests/arith_tests-p503.c
@@ -11,7 +11,7 @@
 
 
 // Benchmark and test parameters  
-#if defined(OPTIMIZED_GENERIC_IMPLEMENTATION) || (TARGET == TARGET_ARM) || (TARGET == TARGET_ARM64) 
+#if defined(OPTIMIZED_GENERIC_IMPLEMENTATION) || (TARGET == TARGET_ARM) 
     #define BENCH_LOOPS           100       // Number of iterations per bench
     #define SMALL_BENCH_LOOPS     100       // Number of iterations per bench
     #define TEST_LOOPS             10       // Number of iterations per test
